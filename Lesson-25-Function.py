fb1ca969d10ea203638648417a4ac96a5e67b0d6
shanto das <114013679+Shanto-123@users.noreply.github.com>
2024-09-30 01:54:48 +0600

    Add files via upload

---
+++ Lesson-25-Function.py
@@ -0,0 +1,9 @@
+def Total_cost(price,shipping,discount):
+     print(f"Total cost : ${price +shipping-discount}")
+
+Total_cost(15,3,2)
+
+def welcome(name, last_name):
+     print(f"Hii! {name} {last_name}")
+welcome("Shanto", "Chandra Das")    
+
